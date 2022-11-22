8b8bfeee806b2ab276706fb4b954a0fbb9a493f1
ibrahim <ibrahimyaacob92@yahoo.com>
2022-11-23 00:50:54 +0800

    two content cards dummy

--- components/AppShellMantine.tsx
+++ components/AppShellMantine.tsx
@@ -1,41 +1,41 @@
 import {
-    AppShell,
-    Header,
-    Footer,
-    Text,
-    useMantineTheme,
+	AppShell,
+	Header,
+	Footer,
+	Text,
+	useMantineTheme,
 } from '@mantine/core';
 import { ReactNode } from 'react';
 
 type Props = {
-    children: ReactNode
+	children: ReactNode
 }
 
 function AppShellMantine({ children }: Props) {
-    const theme = useMantineTheme();
-    return (
-        <AppShell
-            styles={{
-                main: {
-                    background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-                },
-            }}
-            footer={
-                <Footer height={60} p="md">
-                    Application footer
-                </Footer>
-            }
-            header={
-                <Header height={{ base: 50, md: 70 }} p="md">
-                    <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-                        <Text>Application header</Text>
-                    </div>
-                </Header>
-            }
-        >
-            {children}
-        </AppShell>
-    );
+	const theme = useMantineTheme();
+	return (
+		<AppShell
+			styles={{
+				main: {
+					background: theme.colors.violet[0],
+				},
+			}}
+			footer={
+				<Footer height={60} p="md" withBorder={false} sx={{ background: theme.colors.violet[0] }}>
+					Application footer
+				</Footer>
+			}
+			header={
+				<Header height={{ base: 50, md: 70 }} p="md" withBorder={false} sx={{ background: theme.colors.violet[0] }}>
+					<div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+						<Text color="grape.9" weight={500}>MERRYBERRY</Text>
+					</div>
+				</Header>
+			}
+		>
+			{children}
+		</AppShell>
+	);
 }
 
 export default AppShellMantine;
--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -1,4 +1,4 @@
-import { Stack, Text } from '@mantine/core'
+import { Stack, Text, Group, Paper } from '@mantine/core'
 import Image from 'next/image'
 import React from 'react'
 
@@ -6,15 +6,32 @@ type Props = {}
 
 const ContentCard = () => {
   return (
-    <Stack align='center'>
-      <Text>Happy Gut, Happy Life !</Text>
-      <Image
-        src="/../banner/berrybasket.jpg"
-        alt="Basket of berries"
-        width={500}
-        height={500}
-      />
-    </Stack>
+    <Stack>
+      <Stack align='center'>
+        <Text size={40}>Happy Gut, Happy Life !</Text>
+        <Image
+          src="/banners/berrybasket.jpg"
+          alt="Basket of berries"
+          width={500}
+          height={300}
+        />
+      </Stack>
+      {/* <Paper mt="10vh" > */}
+      <div color='violet.9'>
+        <Stack align='center' >
+          <Group position='apart'>
+            <Text size={32}>{"Let's change your lifestyle by consuming this"}</Text>
+            <Image
+              src="/banners/productsample.png"
+              alt="SuperBerry sample"
+              width={500}
+              height={500}
+            />
+          </Group>
+        </Stack>
+      </div>
+      {/* </Paper> */}
+    </Stack >
   )
 }
 
